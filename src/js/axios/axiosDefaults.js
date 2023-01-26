271e2be72795053c80c17815cdc298bd102fc6f5
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-26 13:33:44 +0100

    add dev links for favorite and read pages

--- src/js/axios/axiosDefaults.js
+++ src/js/axios/axiosDefaults.js
@@ -1,1 +1,14 @@
 const WEATHER_KEY = 'f3ada08f24524eaebe0dab7657f9578c';
+
+const axiosWeather = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+  timeout: 1000,
+});
+
+const axiosWeatherGeocoding = axios.create({
+  baseURL:
+    'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}',
+  timeout: 1000,
+});
+
+export { axiosWeather };
