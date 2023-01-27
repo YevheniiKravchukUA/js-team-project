f03baaf25ce8078405ad5a586050e17e1a14f0fb
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-27 10:26:35 +0100

    finish axios defaults settings

--- src/js/requests/newsFetch.js
+++ src/js/requests/newsFetch.js
@@ -1,1 +1,14 @@
 import { axiosNews } from '../axios/axiosDefaults';
+
+async function getNews() {
+  const path = {
+    mostPopular: '/shared/1/facebook.json',
+    articles: 'search/v2/articlesearch.json',
+    allCategories: 'news/v3/content/section-list.json',
+    category: 'news/v3/content/section-list.json',
+  };
+
+  axiosNews.get(mostPopular, {});
+}
+
+export { getNews };
