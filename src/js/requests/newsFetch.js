85273f525e0dba66f8ff65b3ed1ac8be09355e36
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-27 10:50:59 +0100

    add fetch for mostPopular news

--- src/js/requests/newsFetch.js
+++ src/js/requests/newsFetch.js
@@ -1,14 +1,18 @@
 import { axiosNews } from '../axios/axiosDefaults';
 
-async function getNews() {
+async function getNews(category, parameters) {
   const path = {
-    mostPopular: '/shared/1/facebook.json',
+    mostPopular: 'mostpopular/v2/emailed/7.json',
     articles: 'search/v2/articlesearch.json',
     allCategories: 'news/v3/content/section-list.json',
     category: 'news/v3/content/section-list.json',
   };
 
-  axiosNews.get(mostPopular, {});
+  const response = await axiosNews.get(path[category], {
+    params: parameters,
+  });
+
+  return response;
 }
 
 export { getNews };
