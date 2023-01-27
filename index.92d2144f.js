c205b1e0458ed4883e6f5061a0942918a71e4961
YevheniiKravchukUA <115158804+YevheniiKravchukUA@users.noreply.github.com>
2023-01-27 22:04:49 +0000

    Deploying to gh-pages from @ YevheniiKravchukUA/js-team-project@e4e6f70397280c1643419312753c7f29597be121 🚀

---
+++ index.92d2144f.js
@@ -0,0 +1,2 @@
+var activeBtn=document.querySelector(".categories_btn"),showMore=document.querySelector(".categories_btn"),modal=document.querySelector(".btn_group_list");showMore.addEventListener("click",(function(){modal.classList.contains("visible")?(modal.classList.remove("visible"),activeBtn.classList.remove("categories_btn_active")):(modal.classList.add("visible"),activeBtn.classList.add("categories_btn_active"))}));
+//# sourceMappingURL=index.92d2144f.js.map
