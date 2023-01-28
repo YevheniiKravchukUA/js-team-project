1937369f53bc80e9e80b476c5c00e79eded8e71f
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-28 08:41:28 +0100

    fix path and imports

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,4 +1,4 @@
-// import { Datepicker } from 'vanillajs-datepicker';
+import { Datepicker } from 'vanillajs-datepicker';
 
 const calendar = document.querySelector('.calendar__btn');
 const datepicker = new Datepicker(calendar, {
