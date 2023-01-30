c09a9f976c510a501c1d3314bb54f4aca6a8659d
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-30 09:15:28 +0200

    updated check to local storage

--- src/js/favorit/favoriteToLocalStorage.js
+++ src/js/favorit/favoriteToLocalStorage.js
@@ -1,22 +1,43 @@
 // const newsListElements = document.querySelector('.news__list')
 // const favoriteListEl = document.querySelector('.favorite-list')
-// console.log(favoriteListEl)
+// // console.log(favoriteListEl)
 
 
-// let objectValues = { markup: '' }
-// let array =[]
 
-//  console.log(localStorage.getItem("added to favorite"))
+// let arrayOfFavoriteNews =[]
 
 
 // function addRemoveToLocalStore(event) {
-// objectValues= `<div><li>dcdcdcdcdc</li></div>`
-//     const stringifidValues = JSON.stringify(objectValues)
-//     array.push(stringifidValues)
-//     localStorage.setItem("added to favorite", array)
-//     console.log(array)
-//     const markup = array.map(item => item).join('')
-//     // newsListElements.innerHTML = markup
+
+
+//     arrayOfFavoriteNews.push({
+//         image: event.target.parentNode.firstElementChild.src,
+//         title: event.target.parentNode.parentNode.children[2].textContent,
+//         text: event.target.parentNode.parentNode.children[3].textContent,
+//         href: event.target.parentNode.parentNode.lastElementChild.lastElementChild.href,
+//         date: event.target.parentNode.parentNode.lastElementChild.firstElementChild.textContent,
+//         id:event.target.dataset
+        
+//     })
+//     localStorage.setItem('news-added-to-favorite', JSON.stringify(arrayOfFavoriteNews));
+    
+
+//     if (event.target.classList.contains('div')) {
+//         const arrayWithremovedNew = JSON.parse(localStorage.getItem('news-added-to-favorite')).filter(
+//       item => item.id !== e.target.dataset.id
+//     );
+//         localStorage.removeItem('news-added-to-favorite');
+//         localStorage.setItem('news-added-to-favorite', JSON.stringify(arrayWithremovedNew));
+//         arrayOfFavoriteNews=arrayWithremovedNew
 // }
 
-// newsListElements.addEventListener('click',addRemoveToLocalStore)
+// }
+
+
+
+
+// newsListElements.addEventListener('click', addRemoveToLocalStore)
+
+
+
+
