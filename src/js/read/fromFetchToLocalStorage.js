9e60e5a35f6c8d837390ce6f8432cff3383b0f6c
Alexuber <alexuberbuber@gmail.com>
2023-02-01 16:39:09 +0200

    delete no-need code

--- src/js/read/fromFetchToLocalStorage.js
+++ src/js/read/fromFetchToLocalStorage.js
@@ -2,9 +2,9 @@ import { alreadyRead } from './add-to-read';
 
 export function addFetchedToLocalStorage(response) {
   const json = alreadyRead.getJsonFromLocalStorage('NewsFromHome');
-  const news = alreadyRead.dataFromLocalStorage(json); // получаем популярные новости из локалС
+  const news = alreadyRead.dataFromLocalStorage(json);
   if (news !== null) {
-    alreadyRead.newsArr = [...news, ...response]; // записываем их для поиска
+    alreadyRead.newsArr = [...news, ...response];
     localStorage.setItem('NewsFromHome', JSON.stringify(alreadyRead.newsArr));
   } else {
     localStorage.setItem('NewsFromHome', JSON.stringify(response));
